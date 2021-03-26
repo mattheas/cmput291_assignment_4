5584b22b45178c02c5f83d11001fcbd6f5c39e16
Mattheas Jamieson <mattheas@ualberta.ca>
2021-03-26 00:19:13 -0600

    fixed populating databases so they execute in <30 seconds

--- A4P3.py
+++ A4P3.py
@@ -31,6 +31,18 @@ def extract_data(list_countries, list_part_ids):
     list_part_ids = list(dict.fromkeys(list_part_ids))
     print("length of list with duplicates removed = {}".format(len(list_part_ids)))
 
+    # now remove floats and integers BIGGER than 8 bytes(2^63 -1) 
+    list_part_ids_int=[]
+    for num in list_part_ids:
+        try:
+            int_test=int(num)
+            if int_test <= 9223372036854775807:
+                list_part_ids_int.append(num)
+        except ValueError:
+            pass
+    
+    print("length of list with floats/ and not integers removed = {}".format(len(list_part_ids_int)))
+
     
     # now remove country name abbreviations from second file
     with open('data_csv.csv') as csv_file:
@@ -48,117 +60,71 @@ def extract_data(list_countries, list_part_ids):
                 line_count += 1
                 list_countries.append(row[1])
 
-    print("length of list = {}".format(len(list_countries)))  
+    print("length of list of countries = {}".format(len(list_countries)))  
 
-    return list_countries, list_part_ids
+    return list_countries, list_part_ids_int
+
+
+
 
 def populate_db(list_countries, list_part_ids):
 
-    counter_100, counter_1k, counter_10k, counter_100k, counter_1M = 0, 0, 0, 0, 0
+    #counter_100, counter_1k, counter_10k, counter_100k, counter_1M = 0, 0, 0, 0, 0
 
     # define INSERT query
-    sql_select_query = """INSERT INTO Parts (partNumber, partPrice, needsPart, madeIn)
+    sql_insert_query = """INSERT INTO Parts (partNumber, partPrice, needsPart, madeIn)
     VALUES(?,?,?,?);"""
 
-
-    #shuffle all part #'s/ countries once for DB
-    random.shuffle(list_countries)
-    random.shuffle(list_part_ids)
-
     # copy partNumber list so we can remove partNumbers from it after they're inserted into DB
-    temp_part_num_list = list_part_ids.copy()
+    #temp_part_num_list = list_part_ids.copy()
 
-    x = 99
-
-    # 0 to 99
-    while x>=0:
-        print(x)
-        # generate random int for price for each part
-        rand_price = random.randint(1,100)
-
-        # generate random num to index list of partNumbers, partNeeds and madeIn
-        rand_index = random.randint(0,(len(temp_part_num_list)-1))
-        rand_index2 = random.randint(0,(len(list_part_ids)-1))
-        rand_index3 = random.randint(0,(len(list_countries)-1))
-
-        # save random partNumber from list
-        temp_partNumber = temp_part_num_list[rand_index]
-
-        # remove selected partNumber from templist so its not selected againx
-        temp_part_num_list.remove(temp_partNumber)
-
-        try:
-            c1.execute(sql_select_query, (int(temp_partNumber), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]) ))
-            x-=1
-        except:
-            print("error")
+    
 
 
     #shuffle all part #'s/ countries once for DB
     random.shuffle(list_countries)
     random.shuffle(list_part_ids)
 
-    # copy partNumber list so we can remove partNumbers from it after they're inserted into DB
-    temp_part_num_list = list_part_ids.copy()
+    # loop variables/ list
+    error_count = 0
+    x = 0
+    list_of_rows =[]
 
-    x = 999
+    while x<=99:
+        #print(x)
 
-    # 0 to 999
-    while x>=0:
-        
         # generate random int for price for each part
         rand_price = random.randint(1,100)
 
         # generate random num to index list of partNumbers, partNeeds and madeIn
-        rand_index = random.randint(0,(len(temp_part_num_list)-1))
         rand_index2 = random.randint(0,(len(list_part_ids)-1))
         rand_index3 = random.randint(0,(len(list_countries)-1))
 
-        # save random partNumber from list
-        temp_partNumber = temp_part_num_list[rand_index]
-
-        # remove selected partNumber from templist so its not selected againx
-        temp_part_num_list.remove(temp_partNumber)
+        temp_tuple = ()
 
         try:
-            c2.execute(sql_select_query, (int(temp_partNumber), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]) ))
-            x-=1
+            temp_tuple = (int(list_part_ids[x]), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]))
+            list_of_rows.append(temp_tuple)
+            x+=1
+
         except:
             print("error")
-        
+            error_count+=1
 
 
-    #shuffle all part #'s/ countries once for DB
-    random.shuffle(list_countries)
-    random.shuffle(list_part_ids)
+    print('error count is', error_count)
 
-    # copy partNumber list so we can remove partNumbers from it after they're inserted into DB
-    temp_part_num_list = list_part_ids.copy()
+    #insert multiple rows in a single query
+    c1.executemany(sql_insert_query, list_of_rows)
 
-    x = 9999
+    print('We have inserted', c1.rowcount, 'records to the table.')
 
-    # 0 to 9999
-    while x>=0:
-        
-        # generate random int for price for each part
-        rand_price = random.randint(1,100)
+    #commit the changes to db			
+    #conn1.commit()
 
-        # generate random num to index list of partNumbers, partNeeds and madeIn
-        rand_index = random.randint(0,(len(temp_part_num_list)-1))
-        rand_index2 = random.randint(0,(len(list_part_ids)-1))
-        rand_index3 = random.randint(0,(len(list_countries)-1))
-
-        # save random partNumber from list
-        temp_partNumber = temp_part_num_list[rand_index]
-
-        # remove selected partNumber from templist so its not selected againx
-        temp_part_num_list.remove(temp_partNumber)
-
-        try:
-            c3.execute(sql_select_query, (int(temp_partNumber), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]) ))
-            x-=1
-        except:
-            print("error")
+    c1.execute("SELECT COUNT (*) FROM Parts")
+    rowcount = c1.fetchone()[0]
+    print (rowcount)
 
 
 
@@ -167,33 +133,47 @@ def populate_db(list_countries, list_part_ids):
     random.shuffle(list_countries)
     random.shuffle(list_part_ids)
 
-    # copy partNumber list so we can remove partNumbers from it after they're inserted into DB
-    temp_part_num_list = list_part_ids.copy()
+    # loop variables/ list
+    error_count = 0
+    x = 0
+    list_of_rows =[]
 
-    x = 99999
-
-    # 0 to 999
-    while x>=0:
-        
+    while x<=999:
         # generate random int for price for each part
         rand_price = random.randint(1,100)
 
         # generate random num to index list of partNumbers, partNeeds and madeIn
-        rand_index = random.randint(0,(len(temp_part_num_list)-1))
         rand_index2 = random.randint(0,(len(list_part_ids)-1))
         rand_index3 = random.randint(0,(len(list_countries)-1))
 
-        # save random partNumber from list
-        temp_partNumber = temp_part_num_list[rand_index]
-
-        # remove selected partNumber from templist so its not selected againx
-        temp_part_num_list.remove(temp_partNumber)
+        temp_tuple = ()
 
         try:
-            c4.execute(sql_select_query, (int(temp_partNumber), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]) ))
-            x-=1
+            temp_tuple = (int(list_part_ids[x]), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]))
+            list_of_rows.append(temp_tuple)
+            x+=1
+
         except:
             print("error")
+            error_count+=1
+
+
+    print('error count is', error_count)
+
+    #insert multiple rows in a single query
+    c2.executemany(sql_insert_query, list_of_rows)
+
+    print('We have inserted', c2.rowcount, 'records to the table.')
+
+    #commit the changes to db			
+    #conn1.commit()
+
+    c2.execute("SELECT COUNT (*) FROM Parts")
+    rowcount = c2.fetchone()[0]
+    print (rowcount)
+
+
+
 
 
 
@@ -202,33 +182,188 @@ def populate_db(list_countries, list_part_ids):
     random.shuffle(list_countries)
     random.shuffle(list_part_ids)
 
-    # copy partNumber list so we can remove partNumbers from it after they're inserted into DB
-    temp_part_num_list = list_part_ids.copy()
+    # loop variables/ list
+    error_count = 0
+    x = 0
+    list_of_rows =[]
 
-    x = 999999
-
-    # 0 to 999
-    while x>=0:
-        print(x)
+    while x<=9999:
         # generate random int for price for each part
         rand_price = random.randint(1,100)
 
         # generate random num to index list of partNumbers, partNeeds and madeIn
-        rand_index = random.randint(0,(len(temp_part_num_list)-1))
         rand_index2 = random.randint(0,(len(list_part_ids)-1))
         rand_index3 = random.randint(0,(len(list_countries)-1))
 
-        # save random partNumber from list
-        temp_partNumber = temp_part_num_list[rand_index]
-
-        # remove selected partNumber from templist so its not selected againx
-        temp_part_num_list.remove(temp_partNumber)
+        temp_tuple = ()
 
         try:
-            c5.execute(sql_select_query, (int(temp_partNumber), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]) ))
-            x-=1
+            temp_tuple = (int(list_part_ids[x]), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]))
+            list_of_rows.append(temp_tuple)
+            x+=1
+
         except:
             print("error")
+            error_count+=1
+
+
+    print('error count is', error_count)
+
+    #insert multiple rows in a single query
+    c3.executemany(sql_insert_query, list_of_rows)
+
+    print('We have inserted', c3.rowcount, 'records to the table.')
+
+    #commit the changes to db			
+    #conn1.commit()
+
+    c3.execute("SELECT COUNT (*) FROM Parts")
+    rowcount = c3.fetchone()[0]
+    print (rowcount)
+
+
+
+
+
+    #shuffle all part #'s/ countries once for DB
+    random.shuffle(list_countries)
+    random.shuffle(list_part_ids)
+
+    # loop variables/ list
+    error_count = 0
+    x = 0
+    list_of_rows =[]
+
+    while x<=99999:
+        # generate random int for price for each part
+        rand_price = random.randint(1,100)
+
+        # generate random num to index list of partNumbers, partNeeds and madeIn
+        rand_index2 = random.randint(0,(len(list_part_ids)-1))
+        rand_index3 = random.randint(0,(len(list_countries)-1))
+
+        temp_tuple = ()
+
+        try:
+            temp_tuple = (int(list_part_ids[x]), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]))
+            list_of_rows.append(temp_tuple)
+            x+=1
+
+        except:
+            print("error")
+            error_count+=1
+
+
+    print('error count is', error_count)
+
+    #insert multiple rows in a single query
+    c4.executemany(sql_insert_query, list_of_rows)
+
+    print('We have inserted', c4.rowcount, 'records to the table.')
+
+    #commit the changes to db			
+    #conn1.commit()
+
+    c4.execute("SELECT COUNT (*) FROM Parts")
+    rowcount = c4.fetchone()[0]
+    print (rowcount)
+
+
+
+    '''
+
+
+    #shuffle all part #'s/ countries once for DB
+    random.shuffle(list_countries)
+    random.shuffle(list_part_ids)
+
+    # loop variables/ list
+    error_count = 0
+    x = 0
+    list_of_rows =[]
+
+    while x<=999999:
+        # generate random int for price for each part
+        rand_price = random.randint(1,100)
+
+        # generate random num to index list of partNumbers, partNeeds and madeIn
+        rand_index2 = random.randint(0,(len(list_part_ids)-1))
+        rand_index3 = random.randint(0,(len(list_countries)-1))
+
+        temp_tuple = ()
+
+        try:
+            temp_tuple = (int(list_part_ids[x]), int(rand_price), int(list_part_ids[rand_index2]), str(list_countries[rand_index3]))
+            list_of_rows.append(temp_tuple)
+            x+=1
+
+        except:
+            print("error")
+            error_count+=1
+
+
+    print('error count is', error_count)
+
+    #insert multiple rows in a single query
+    c5.executemany(sql_insert_query, list_of_rows)
+
+    print('We have inserted', c5.rowcount, 'records to the table.')
+
+    #commit the changes to db	  		
+    #conn1.commit()
+
+    c5.execute("SELECT COUNT (*) FROM Parts")
+    rowcount = c5.fetchone()[0]
+    print (rowcount)
+    '''
+
+
+def part3_query(list_countries, list_part_ids):
+
+    # define part3 query
+    sql_select_query = """SELECT p.partNumber, MAX(p.partPrice)
+    FROM Parts p
+    WHERE p.madeIn = ?
+    GROUP BY p.partNumber;"""
+
+    # generate radom country code 
+    rand_country_index = random.randint(0,(len(list_countries)-1))
+    rand_country = list_countries[rand_country_index]
+
+    # execute query to find most expensive part in country code
+    c1.execute(sql_select_query, (str(rand_country),) )
+
+    records = c1.fetchall()
+
+    print(" partNumber   MAX(part price)")
+
+    for row in records:
+        print("{}   {}".format(row[0], row[1]))
+
+
+
+
+
+
+    # query used to check if ACTUAL query is correct
+    sql_select_query1 = """SELECT p.partNumber, p.madeIn
+    FROM Parts p
+    WHERE p.madeIn = ?;"""
+
+    c1.execute(sql_select_query1, (str(rand_country),) )
+
+    records = c1.fetchall()
+
+    print(" partNumber   madeIn")
+
+    for row in records:
+        print("{}   {}".format(row[0], row[1]))
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -244,6 +379,13 @@ if __name__ == "__main__":
     conn5 = sqlite3.connect("A4v1M.db")
     c5 = conn5.cursor()
 
+    # used to clear all rows in table if needed
+    '''
+    sql_update_query = """DELETE from Parts """
+    c1.execute(sql_update_query)
+    conn1.commit()
+    '''
+
     # lists to hold part number ID's and country abbreviations
     country_list = []
     part_id_list = []
@@ -251,7 +393,11 @@ if __name__ == "__main__":
     # extract data and store in lists
     country_list, part_id_list = extract_data(country_list, part_id_list)
 
-    # now populate DB's
-    populate_db(country_list, part_id_list)
+    # now populate DB's randomly
+    #populate_db(country_list, part_id_list)
+
+    # Run part 3
+    part3_query(country_list, part_id_list)
+
 
 

084418fb32e2dcdcc8a6f75cec09b2b8d7e6cce1
Mattheas Jamieson <mattheas@ualberta.ca>
2021-03-26 21:16:28 -0600

    final draft

--- A4P3.py
+++ A4P3.py
@@ -26,11 +26,11 @@ def extract_data(list_countries, list_part_ids):
                     line_count += 1
                     list_part_ids.append(row[0])
                 
-    print("length of list = {}".format(len(list_part_ids)))
+    #print("length of list = {}".format(len(list_part_ids)))
     
     # remove duplicate part numbers because of primary key constraint
     list_part_ids = list(dict.fromkeys(list_part_ids))
-    print("length of list with duplicates removed = {}".format(len(list_part_ids)))
+    #print("length of list with duplicates removed = {}".format(len(list_part_ids)))
 
     # now remove floats and integers BIGGER than 8 bytes(2^63 -1) 
     list_part_ids_int=[]
@@ -42,7 +42,7 @@ def extract_data(list_countries, list_part_ids):
         except ValueError:
             pass
     
-    print("length of list with floats/ and not integers removed = {}".format(len(list_part_ids_int)))
+    #print("length of list with floats/ and not integers removed = {}".format(len(list_part_ids_int)))
 
     
     # now remove country name abbreviations from second file
@@ -61,11 +61,9 @@ def extract_data(list_countries, list_part_ids):
                 line_count += 1
                 list_countries.append(row[1])
 
-    print("length of list of countries = {}".format(len(list_countries)))  
+    #print("length of list of countries = {}".format(len(list_countries)))  
 
     return list_countries, list_part_ids_int
-
-
 
 
 
@@ -110,19 +108,19 @@ def populate_db(list_countries, list_part_ids):
             error_count+=1
 
 
-    print('error count is', error_count)
+    #print('error count is', error_count)
 
     #insert multiple rows in a single query
     c1.executemany(sql_insert_query, list_of_rows)
 
-    print('We have inserted', c1.rowcount, 'records to the table.')
+    #print('We have inserted', c1.rowcount, 'records to the table.')
 
     #commit the changes to db			
     #conn1.commit()
 
     c1.execute("SELECT COUNT (*) FROM Parts")
     rowcount = c1.fetchone()[0]
-    print (rowcount)
+    #print (rowcount)
 
 
 
@@ -156,26 +154,26 @@ def populate_db(list_countries, list_part_ids):
             error_count+=1
 
 
-    print('error count is', error_count)
+    #print('error count is', error_count)
 
     #insert multiple rows in a single query
     c2.executemany(sql_insert_query, list_of_rows)
 
-    print('We have inserted', c2.rowcount, 'records to the table.')
+    #print('We have inserted', c2.rowcount, 'records to the table.')
 
     #commit the changes to db			
     #conn1.commit()
 
     c2.execute("SELECT COUNT (*) FROM Parts")
     rowcount = c2.fetchone()[0]
-    print (rowcount)
+    #print (rowcount)
 
 
 
 
 
 
-
+    
     #shuffle all part #'s/ countries once for DB
     random.shuffle(list_countries)
     random.shuffle(list_part_ids)
@@ -205,19 +203,19 @@ def populate_db(list_countries, list_part_ids):
             error_count+=1
 
 
-    print('error count is', error_count)
+    #print('error count is', error_count)
 
     #insert multiple rows in a single query
     c3.executemany(sql_insert_query, list_of_rows)
 
-    print('We have inserted', c3.rowcount, 'records to the table.')
+    #print('We have inserted', c3.rowcount, 'records to the table.')
 
     #commit the changes to db			
     #conn1.commit()
 
     c3.execute("SELECT COUNT (*) FROM Parts")
     rowcount = c3.fetchone()[0]
-    print (rowcount)
+    #print (rowcount)
 
 
 
@@ -252,19 +250,19 @@ def populate_db(list_countries, list_part_ids):
             error_count+=1
 
 
-    print('error count is', error_count)
+    #print('error count is', error_count)
 
     #insert multiple rows in a single query
     c4.executemany(sql_insert_query, list_of_rows)
 
-    print('We have inserted', c4.rowcount, 'records to the table.')
+    #print('We have inserted', c4.rowcount, 'records to the table.')
 
     #commit the changes to db			
     #conn1.commit()
 
     c4.execute("SELECT COUNT (*) FROM Parts")
     rowcount = c4.fetchone()[0]
-    print (rowcount)
+    #print (rowcount)
 
 
 
@@ -300,51 +298,49 @@ def populate_db(list_countries, list_part_ids):
             error_count+=1
 
 
-    print('error count is', error_count)
+    #print('error count is', error_count)
 
     #insert multiple rows in a single query
     c5.executemany(sql_insert_query, list_of_rows)
 
-    print('We have inserted', c5.rowcount, 'records to the table.')
+    #print('We have inserted', c5.rowcount, 'records to the table.')
 
     #commit the changes to db	  		
     #conn1.commit()
 
     c5.execute("SELECT COUNT (*) FROM Parts")
     rowcount = c5.fetchone()[0]
-    print (rowcount)
+    #print (rowcount)
+    
     
 
 
+def part3_query():
 
-
-
-
-
-def part3_query(list_countries, list_part_ids):
+    print("Executing Part 3")
+    print()
+    print("Executing Task I")
 
     # define part3 query
     sql_select_query = """SELECT p.partNumber, p.madeIn, p.partPrice
     FROM Parts p
     WHERE p.madeIn = ? AND p.partPrice >= (SELECT MAX(p1.partPrice) FROM Parts p1 WHERE p1.madeIn = ?);"""
+    
+    # define query for generating list of countries in db
+    sql_select_countries = """SELECT p.madeIn
+    FROM Parts p;"""
 
 
 
-
-    '''
-    # execute query to find most expensive part in country code
-    c3.execute(sql_select_query, (str(rand_country), str(rand_country)) )
-
-    records = c3.fetchall()
-
-    print(" partNumber   MAX(part price)")
-
+    # generate list of all countries in A4v100.db
+    c1.execute(sql_select_countries)
+    list_countries = []
+    records = c1.fetchall()
     for row in records:
-        print("{}   {}    {}".format(row[0], row[1], row[2]))
-    '''
+        list_countries.append(row[0])   
 
     query_time_100 = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 49 for DB A4v100.db
     for x in range(50):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -358,13 +354,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_100.append(end - start)
       
-    
-    print('average query time of A4v100 in seconds without index = ',sum(query_time_100) / len(query_time_100))
+    print('Average query time of A4v100 in seconds without index = ',sum(query_time_100) / len(query_time_100))
 
 
+
+    # generate list of all countries in A4v1k.db
+    c2.execute(sql_select_countries)
+    list_countries = []
+    records = c2.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_1k = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 49 for DB A4v1k.db
     for x in range(50):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -378,14 +380,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_1k.append(end - start)
       
+    print('Average query time of A4v1k in seconds without index = ',sum(query_time_1k) / len(query_time_1k))
     
-    print('average query time of A4v1k in seconds without index = ',sum(query_time_1k) / len(query_time_1k))
+    
 
-
-
-
+    # generate list of all countries in A4v10k.db
+    c3.execute(sql_select_countries)
+    list_countries = []
+    records = c3.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
+    
     query_time_10k = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 49 for DB A4v10k.db
     for x in range(50):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -398,13 +405,20 @@ def part3_query(list_countries, list_part_ids):
 
         # add query time to list
         query_time_10k.append(end - start)
-      
-    
-    print('average query time of A4v10k in seconds without index = ',sum(query_time_10k) / len(query_time_10k))
+       
+    print('Average query time of A4v10k in seconds without index = ',sum(query_time_10k) / len(query_time_10k))
 
+
+
+    # generate list of all countries in A4v100.db
+    c4.execute(sql_select_countries)
+    list_countries = []
+    records = c4.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_100k = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 9 for DB A4v100k.db
     for x in range(10):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -418,14 +432,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_100k.append(end - start)
       
-    
-    print('average query time of A4v100k in seconds without index = ',sum(query_time_100k) / len(query_time_100k))
+    print('Average query time of A4v100k in seconds without index = ',sum(query_time_100k) / len(query_time_100k))
 
 
 
+    # generate list of all countries in A4v100.db
+    c5.execute(sql_select_countries)
+    list_countries = []
+    records = c5.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_1M = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 4 for DB A4v1M.db
     for x in range(5):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -439,23 +458,34 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_1M.append(end - start)
       
+    print('Average query time of A4v1M in seconds without index = ',sum(query_time_1M) / len(query_time_1M))
     
-    print('average query time of A4v1M in seconds without index = ',sum(query_time_1M) / len(query_time_1M))
 
 
     # define and create index for all 5 db's
-    sql_create_index = '''CREATE INDEX idx_parts_country ON Parts(madeIn);'''
+    sql_create_index = '''CREATE INDEX idx_parts_country ON Parts( partPrice ASC, madeIn );'''
     c1.execute(sql_create_index)
     c2.execute(sql_create_index)
     c3.execute(sql_create_index)
     c4.execute(sql_create_index)
     c5.execute(sql_create_index)
+    
+    print()
+    print("Creating Index")
+    print()
+    print("Executing Task K")
 
 
 
+    # generate list of all countries in A4v100.db
+    c1.execute(sql_select_countries)
+    list_countries = []
+    records = c1.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_100_indexed = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 49 for DB A4v100.db
     for x in range(50):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -469,13 +499,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_100_indexed.append(end - start)
       
-    
-    print('average query time of A4v100 in seconds with index = ',sum(query_time_100_indexed) / len(query_time_100_indexed))
+    print('Average query time of A4v100 in seconds with index = ',sum(query_time_100_indexed) / len(query_time_100_indexed))
 
 
+
+    # generate list of all countries in A4v1k.db
+    c2.execute(sql_select_countries)
+    list_countries = []
+    records = c2.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_1k_indexed = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 49 for DB A4v1k.db
     for x in range(50):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -489,14 +525,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_1k_indexed.append(end - start)
       
+    print('Average query time of A4v1k in seconds with index = ',sum(query_time_1k_indexed) / len(query_time_1k_indexed))
     
-    print('average query time of A4v1k in seconds with index = ',sum(query_time_1k_indexed) / len(query_time_1k_indexed))
 
 
-
-
+    # generate list of all countries in A4v10k.db
+    c3.execute(sql_select_countries)
+    list_countries = []
+    records = c3.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
+    
     query_time_10k_indexed = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 49 for DB A4v10k.db
     for x in range(50):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -510,12 +551,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_10k_indexed.append(end - start)
       
-    
-    print('average query time of A4v10k in seconds with index = ',sum(query_time_10k_indexed) / len(query_time_10k_indexed))
+    print('Average query time of A4v10k in seconds with index = ',sum(query_time_10k_indexed) / len(query_time_10k_indexed))
 
+
+
+    # generate list of all countries in A4v100k.db
+    c4.execute(sql_select_countries)
+    list_countries = []
+    records = c4.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_100k_indexed = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 9 for DB A4v100k.db
     for x in range(10):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -529,14 +577,19 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_100k_indexed.append(end - start)
       
-    
-    print('average query time of A4v100k in seconds with index = ',sum(query_time_100k_indexed) / len(query_time_100k_indexed))
+    print('Average query time of A4v100k in seconds with index = ',sum(query_time_100k_indexed) / len(query_time_100k_indexed))
 
 
 
+    # generate list of all countries in A4v1M.db
+    c5.execute(sql_select_countries)
+    list_countries = []
+    records = c5.fetchall()
+    for row in records:
+        list_countries.append(row[0]) 
 
     query_time_1M_indexed = []
-    # values 0 to 99 for DB A4v100.db
+    # values 0 to 99 for DB A4v1M.db
     for x in range(5):
         # generate radom country code 
         rand_country_index = random.randint(0,(len(list_countries)-1))
@@ -550,39 +603,7 @@ def part3_query(list_countries, list_part_ids):
         # add query time to list
         query_time_1M_indexed.append(end - start)
       
-    
-    print('average query time of A4v1M in seconds with index = ',sum(query_time_1M_indexed) / len(query_time_1M_indexed))
-
-
-
-
-
-
-
-
-
-
-
-    # query below used to test if part3 query is working correctly
-    '''
-    # query used to check if ACTUAL query is correct
-    sql_select_query1 = """SELECT p.partNumber, p.madeIn, p.partPrice
-    FROM Parts p
-    WHERE p.madeIn = ?;"""
-
-    c3.execute(sql_select_query1, (str(rand_country),) )
-
-    records = c3.fetchall()
-
-    print(" partNumber   madeIn")
-
-    for row in records:
-        print("{}   {}   {}".format(row[0], row[1], row[2]))
-    '''
-
-
-
-
+    print('Average query time of A4v1M in seconds with index = ',sum(query_time_1M_indexed) / len(query_time_1M_indexed))
 
 
 
@@ -628,7 +649,7 @@ if __name__ == "__main__":
     c4.execute(sql_drop_query)
     c5.execute(sql_drop_query)
 
-    
+
 
     # lists to hold part number ID's and country abbreviations
     country_list = []
@@ -641,7 +662,7 @@ if __name__ == "__main__":
     populate_db(country_list, part_id_list)
 
     # Run part 3
-    part3_query(country_list, part_id_list)
+    part3_query()
 
 
 
